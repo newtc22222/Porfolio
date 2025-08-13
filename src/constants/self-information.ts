40948fa6de2cb959390ae64cdde3b8e83f9480f3
Phi Võ <75079929+sunflynf@users.noreply.github.com>
2025-08-13 23:14:41 +0700

    Update personal information in self-information.ts

--- src/constants/self-information.ts
+++ src/constants/self-information.ts
@@ -1,9 +1,9 @@
 export const FULLNAME = 'VO NHAT PHI';
 export const JOB_TITLE = 'Fullstack Web Developer';
-export const EMAIL = 'nhatphi1598753@gmail.com';
+export const EMAIL = 'phi.vo.tech@gmail.com';
 
 export const SOCIAL_LINKS = {
-  LINKEDIN: 'https://www.linkedin.com/in/vo-nhat-phi-5005a821b/',
+  LINKEDIN: 'https://www.linkedin.com/in/phi-vo-tech/',
   GITHUB: 'https://github.com/newtc22222',
   GITLAB: 'https://gitlab.com/newtc22222',
   FACEBOOK: 'https://www.facebook.com/fee.fight',
