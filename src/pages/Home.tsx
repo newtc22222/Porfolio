c0da828c8a697432a6a093e9202b3a08d3090196
Phi Vo <sunflynf@gmail.com>
2026-07-11 23:58:40 +0700

    fix: replace config of tsparticles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Element, Link } from 'react-scroll';
-import Particles, { initParticlesEngine } from '@tsparticles/react';
+import Particles, { ParticlesProvider } from '@tsparticles/react';
 import { loadSlim } from '@tsparticles/slim';
-
+import type { Engine, ISourceOptions } from '@tsparticles/engine';
 import { FULLNAME, JOB_TITLE, GREETINGS } from '../constants/self-information';
 
+const particlesInit = async (engine: Engine): Promise<void> => {
+  await loadSlim(engine);
+};
+
 export const Home = () => {
-  const [init, setInit] = useState(false);
   const [greetingPrefix] = useState(() => {
     const hour = new Date().getHours();
     let timeGreeting = 'Good day! ☀️';
@@ -25,13 +28,7 @@ export const Home = () => {
     return allGreetings[randomIndex];
   });
 
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      await loadSlim(engine);
-    }).then(() => setInit(true));
-  }, []);
-
-  const options = useMemo(
+  const options: ISourceOptions = useMemo(
     () => ({
       fullScreen: {
         enable: false,
@@ -82,32 +79,32 @@ export const Home = () => {
         id="home"
         className="bg-background-light dark:bg-background-dark relative mt-[64px] flex min-h-screen items-center justify-center overflow-hidden"
       >
-        {init && (
+        <ParticlesProvider init={particlesInit}>
           <Particles
             id="tsparticles"
             options={options}
             className="absolute inset-0 h-full w-full"
           />
-        )}
-        <div className="z-10 text-center">
-          <h1 className="text-primary-light dark:text-primary-dark my-2 text-3xl font-bold">
-            {greetingPrefix}
-          </h1>
-          <p className="text-primary-light dark:text-primary-dark my-4 text-5xl font-bold">
-            I&apos;m {FULLNAME}
-          </p>
-          <p className="text-secondary-light dark:text-secondary-dark my-8 text-2xl">
-            {JOB_TITLE}
-          </p>
-          <Link
-            smooth
-            duration={500}
-            to="#projects"
-            className="border-primary-light dark:border-primary-dark text-primary-light dark:text-primary-dark hover:bg-secondary-light hover:border-secondary-light dark:hover:bg-secondary-dark dark:hover:border-secondary-dark rounded-lg border-2 px-6 py-3 transition-all duration-300 hover:cursor-pointer hover:text-white dark:hover:text-white"
-          >
-            Explore My Projects
-          </Link>
-        </div>
+          <div className="z-10 text-center">
+            <h1 className="text-primary-light dark:text-primary-dark my-2 text-3xl font-bold">
+              {greetingPrefix}
+            </h1>
+            <p className="text-primary-light dark:text-primary-dark my-4 text-5xl font-bold">
+              I&apos;m {FULLNAME}
+            </p>
+            <p className="text-secondary-light dark:text-secondary-dark my-8 text-2xl">
+              {JOB_TITLE}
+            </p>
+            <Link
+              smooth
+              duration={500}
+              to="#projects"
+              className="border-primary-light dark:border-primary-dark text-primary-light dark:text-primary-dark hover:bg-secondary-light hover:border-secondary-light dark:hover:bg-secondary-dark dark:hover:border-secondary-dark rounded-lg border-2 px-6 py-3 transition-all duration-300 hover:cursor-pointer hover:text-white dark:hover:text-white"
+            >
+              Explore My Projects
+            </Link>
+          </div>
+        </ParticlesProvider>
       </section>
     </Element>
   );
